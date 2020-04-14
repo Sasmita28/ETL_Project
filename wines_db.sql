3efdecc7586e0093bb470de29aa84f2a34adf565
KotR9001 <57807780+KotR9001@users.noreply.github.com>
2020-04-13 22:22:16 -0500

    Make changes to file.

--- wines_db.sql
+++ wines_db.sql
@@ -1,4 +1,5 @@
 drop table if exists wines_table;
+drop table if exists wines_titles_table;
 
 create table wines_table (
 	id int primary key,
@@ -13,4 +14,11 @@ create table wines_table (
 	vintage varchar
 );
 
+create table wines_titles_table (
+	title varchar,
+	points int,
+	price money
+);
+
 select * from wines_table;
+select * from wines_titles_table;
